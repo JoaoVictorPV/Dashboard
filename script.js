4da3b0e07d450b7c55f35a7da2c183e9d4a434fb
JoaoVictorPV <joaovictorpv@hotmail.com>
2025-09-26 22:07:53 -0300

    feat: Substitui caracteres de Open Access por ícones SVG

--- script.js
+++ script.js
@@ -273,8 +273,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Ícone de Open Access
             const isOpenAccess = article['OPEN ACESS'] === 'Sim';
             const openAccessIcon = isOpenAccess 
-                ? '<span class="open-access-icon-v">V</span>' 
-                : '<span class="open-access-icon-x">X</span>';
+                ? `<svg class="icon-v" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`
+                : `<svg class="icon-x" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>`;
             
             row.innerHTML = `
                 <td class="col-date">${formatDate(article['DATA DE PUBLICACAO'])}</td>
